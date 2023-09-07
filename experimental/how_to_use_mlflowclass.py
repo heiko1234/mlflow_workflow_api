1def2b10f0a18247f133f7562e69bb52f2738375
Heiko Kulinna <Heikokulinna@gmx.de>
2023-09-07 23:03:29 +0200

    [HK] update on new model

--- experimental/how_to_use_mlflowclass.py
+++ experimental/how_to_use_mlflowclass.py
@@ -9,15 +9,30 @@ import pandas as pd
 df = pd.DataFrame()
 
 df["Yield"] = [44.0, 43.0, 46.0, 40.1, 42.2]
+
+
 df["BioMaterial1"]=[5.5, 4.5, 3.5, 1.0, 6.0]
 df["BioMaterial2"]=[9.5, 9, 5, 10, 12]
 df["ProcessValue1"] = [20, 15, 10, 9, 2]
+
+
+
+
+df["BiologicalMaterial02"] = [55, 54, 48, 58, 60.5]
+df["BiologicalMaterial06"] = [40.9, 55.5, 50.5, 60.5, 60.5]
+df["ManufacturingProcess06"] = [200, 215, 210, 209, 202]
+
+
+df
+
 
 
 target = "Yield"
 
 features = ["BioMaterial1", "BioMaterial2", "ProcessValue1"]
 
+
+features = ["BiologicalMaterial02", "BiologicalMaterial06", "ManufacturingProcess06"]
 
 
 # /home/heiko/Schreibtisch/Repos/dash_apps/mlflow_workflow_app/experimental/mflow_workflow/how_to_use_mlflowclass.py
