8207a7ce6bf5935f97eae7c8ec610f236abc7405
Heiko Kulinna <Heikokulinna@gmx.de>
2023-09-18 15:19:20 +0200

    [HK] added some logging

--- backend_service/utilities/mlflow_training_class.py
+++ backend_service/utilities/mlflow_training_class.py
@@ -47,26 +47,26 @@ from dotenv import load_dotenv
 
 
 class mlflow_training():
-    
+
     def __init__(self, model_name=None):
-        
+
         load_dotenv()
         self.model_name = model_name
 
 
     def descriptiontable(self, data):
-        
+
         dft=data.describe().reset_index(drop = True).T
         dft = dft.reset_index(drop=False)
         dft.columns= ["description", "counts", "mean", "std", "min", "25%", "50%", "75%", "max"]
         dft["nan"]=data.isna().sum().values
-        
+
         return dft
-    
+
     def transform_rawdata(self, data, transformation_dict=None):
-        
+
         output = data.copy()
-        
+
         if transformation_dict == None:
             print("No transformation dict given!")
         for column in output.columns:
@@ -77,11 +77,11 @@ class mlflow_training():
                 print(e)
                 print(f"Column {column} not transformed! {column} is not in transformation_dict")
                 pass
-        
+
         return output
-    
+
     def transform_column(self, data, column, transformation):
-        
+
         if transformation == "no transformation":
             data_series = data[column]
         elif transformation == "log":
@@ -96,10 +96,10 @@ class mlflow_training():
             data_series = data[column].apply(lambda x: x**3)
         else:
             data_series = data[column]
-            
+
         return data_series
-    
-    
+
+
     def update_nested_dict(self, original_dict, overwrite_dict):
         """This function updates a nested dictionary
 
@@ -109,19 +109,19 @@ class mlflow_training():
         Returns:
             _type_: returns the original_dict updated with the overwrite_dict
         """
-    
-    
+
+
         for k, v in overwrite_dict.items():
             if isinstance(v, collections.abc.Mapping):
                 original_dict[k] = self.update_nested_dict(original_dict.get(k, {}), v)
             else:
                 original_dict[k] = v
-                
+
         return original_dict
-        
-    
+
+
     def descriptiontable_with_correlation(self, data, target):
-        
+
         correlations = []
         for i in data.columns:
             if i != target:
@@ -132,26 +132,26 @@ class mlflow_training():
                     correlations.append(0)
             else:
                 correlations.append(1)
-                
+
         correlations = [round(x, 4) for x in correlations]
-        
+
         dft = self.descriptiontable(data)
         dft["correlation"] = correlations
-        
+
         return dft
 
 
     def make_minmaxscalingtable_by_descriptiontable(self, descriptiontable, expand_by=None):
-        
+
         output_df = pd.DataFrame()
-        
+
         if expand_by == None:
-            
+
             for row_index in range(descriptiontable.shape[0]):
                 output_df[descriptiontable.loc[row_index, "description"]] = [descriptiontable.loc[row_index, "max"], descriptiontable.loc[row_index, "min"]]
 
         elif expand_by == "std":
-                
+
                 for row_index in range(descriptiontable.shape[0]):
                     output_df[descriptiontable.loc[row_index, "description"]] = [descriptiontable.loc[row_index, "max"]+ descriptiontable.loc[row_index, "std"], descriptiontable.loc[row_index, "min"]- descriptiontable.loc[row_index, "std"]]
 
@@ -159,21 +159,21 @@ class mlflow_training():
 
 
     def create_data_minmax_dict(self, data):
-    
+
         if isinstance(data, pd.DataFrame):
 
             feature_data_minmax = data.describe().loc[["min", "max"], :]
 
             output = feature_data_minmax.to_dict()
-        
+
         else:
             output = {data.name: {"min": data.min(), "max": data.max()}}
-        
+
         return output
 
 
     def create_data_dtype_dict(self, data):
-        
+
         pandas_dtypes = {
             "float64": "float",
             "int64": "integer",
@@ -192,7 +192,7 @@ class mlflow_training():
 
 
     def create_signature(self, data, target, features):
-        
+
         pandas_dtypes = {
             "float64": "float",
             "int64": "integer",
@@ -201,7 +201,7 @@ class mlflow_training():
             "object": "string",
             "binary": "binary",
             }
-        
+
         input_schema = Schema(
             [
                 ColSpec(
@@ -220,8 +220,8 @@ class mlflow_training():
 
 
     def mlflow_training(self, features_train, target_train, features_test, target_test, signature, data_minmax_dict, feature_minmax_dict_original, feature_minmax_dict, target_minmax_dict, feature_dtypes_dict, model_name, transformation_dict=None, model_typ="linear_regression"):
-        
-        
+
+
         if model_name is not None:
             model_name = model_name
         else:
@@ -238,19 +238,19 @@ class mlflow_training():
             "elastic_net": ElasticNet(),
             "ada_boost": AdaBoostRegressor()
             }
-        
+
         sk_model = sk_model[model_typ]
         print(sk_model)
-        
-        
+
+
         mlflow.set_experiment(model_name)
         print(f"MLFLow is tracking: {mlflow.is_tracking_uri_set()}")
-        
-        
+
+
         with mlflow.start_run():
-            
+
             sk_model.fit(features_train, target_train)
-            
+
             train_score = round(sk_model.score(features_train, target_train), 4)
             test_score = round(sk_model.score(features_test, target_test), 4)
 
@@ -260,6 +260,27 @@ class mlflow_training():
             mlflow.log_metric("train_score", train_score)
             mlflow.log_metric("test_score", test_score)
 
+
+            # new
+
+            df_predictions = sk_model.predict(features_test)
+            predicted_values = list(df_predictions.flatten())
+            target_test_values = list(target_test.flatten())
+
+            rmse = np.sqrt(mean_squared_error(target_test_values, predicted_values))
+            mae = mean_absolute_error(target_test_values, predicted_values)
+            r2 = r2_score(target_test_values, predicted_values)
+
+            mlflow.log_metric("rmse", rmse)
+            mlflow.log_metric("mae", mae)
+            mlflow.log_metric("r2", r2)
+
+            # tags
+            mlflow.set_tag("model_typ", model_typ)
+            mlflow.set_tag("target", list(target_minmax_dict.keys())[0])
+            mlflow.set_tag("features", list(feature_minmax_dict.keys()))
+
+            # artifacts
             mlflow.sklearn.log_model(sk_model, "model", signature=signature)
 
 
@@ -278,82 +299,80 @@ class mlflow_training():
             mlflow.log_dict(
                 feature_dtypes_dict, "model/feature_dtypes.json"
             )
-            
+
             mlflow.log_dict(
                 transformation_dict, "model/transformation_dict.json"
             )
-            
+
         mlflow.end_run()
 
 
 
     def make_model(self, data=None, target=None, features=None, test_size = 0.2, scaler_expand_by="std", transformation_dict=None, model_name=None, model_parameter=None, model_typ="linear_regression"):
-        
 
 
-        
+
         try:
-        
+
             target_and_features = [target] + features
-            
+
             df_tf = data[target_and_features].copy()
-            
-            
+
+
             df_tf_transformed = self.transform_rawdata(data=df_tf, transformation_dict=transformation_dict)
             dft = self.descriptiontable(df_tf_transformed)
-            
+
             dft_original = self.descriptiontable(df_tf)
-            
-            
+
+
 
             new_minmaxscalingdf_scaled = self.make_minmaxscalingtable_by_descriptiontable(
-                descriptiontable=dft, 
+                descriptiontable=dft,
                 expand_by=scaler_expand_by)
-            
-            
+
+
             new_minmaxscalingdf_original = self.make_minmaxscalingtable_by_descriptiontable(
-                descriptiontable=dft_original, 
+                descriptiontable=dft_original,
                 expand_by=None)
 
 
             # target_minmaxscaling = list(target_minmaxscaling)
             # target_minmax_list = np.array(target_minmax_list)
             # target_minmax_list = target_minmax_list.reshape(-1, 1)
-            
-            
+
+
             # scaled
             data_minmax_dict_scaled = self.create_data_minmax_dict(new_minmaxscalingdf_scaled)
             feature_minmax_dict_scaled = self.create_data_minmax_dict(new_minmaxscalingdf_scaled.loc[:, features])
             target_minmax_dict_scaled = self.create_data_minmax_dict(new_minmaxscalingdf_scaled.loc[:, target])
-            
+
             # original
             feature_minmax_dict_original = self.create_data_minmax_dict(new_minmaxscalingdf_original.loc[:, features])
-            
-            
+
+
             feature_dtypes_dict = self.create_data_dtype_dict(new_minmaxscalingdf_scaled.loc[:,features])
-            
-            
+
+
+
             print("MinMaxScaler Features")
             feature_minmaxscaler = MinMaxScaler()
             feature_minmaxscaler.fit(new_minmaxscalingdf_scaled.loc[:, features])
             fitted_df_features = feature_minmaxscaler.transform(df_tf_transformed.loc[:,features])
 
             # print(f"fitted_df_features: {fitted_df_features}")
-            
-            
-            
+
+
             print("MinMaxScaler Target")
             target_minmaxscaler = MinMaxScaler()
             target_minmax_list = list(new_minmaxscalingdf_scaled.loc[:, target])
             target_minmax_list = np.array(target_minmax_list)
             target_minmax_list = target_minmax_list.reshape(-1, 1)
-            
-            
+
+
             target_minmaxscaler.fit(target_minmax_list)
             fitted_target = target_minmaxscaler.transform(df_tf_transformed.loc[:,[target]])
-            
-            # print(f"fitted_target: {fitted_target}")         
 
+            # print(f"fitted_target: {fitted_target}")
 
             print("Create Signature")
             signature = self.create_signature(data=data, target=target, features=features)
@@ -361,7 +380,7 @@ class mlflow_training():
 
 
             print("Train Test Split")
-            random_state = 2023 
+            random_state = 2023
             # test_size =0.2
 
             (
@@ -398,14 +417,14 @@ class mlflow_training():
                 model_name=MLFlow_Experiment,
                 model_typ=model_typ
                 )
-            
+
             print("Model created!")
 
 
         except Exception as e:
             print(e)
             print("Model not created!")
-    
+
         return "Done!"
 
 
